3e5cb13f3ffc7912458bd36c758706456418c1ad
kyechan99 <kyechan99@gmail.com>
2023-11-13 18:34:20 +0900

    test: bundle-analyzer

--- next.config.js
+++ next.config.js
@@ -6,6 +6,44 @@ const nextConfig = {
     // ssr and displayName are configured by default
     styledComponents: true,
   },
-}
+};
 
-module.exports = nextConfig
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+  openAnalyzer: false,
+});
+ 
+
+module.exports = withBundleAnalyzer(nextConfig);
+
+
+// // import withBundleAnalyzer from '@next/bundle-analyzer';
+// const withBundleAnalyzer = require("@next/bundle-analyzer")({
+//   enabled: process.env.ANALYZE === "true",
+//   openAnalyzer: true,
+// });
+
+// // const bundleAnalyzer = withBundleAnalyzer({
+// // 	enabled: process.env.ANALYZE === 'true',
+// //   openAnalyzer: true,
+// // })
+// // module.exports = nextConfig
+// module.exports = withBundleAnalyzer({
+//   compress: true,
+//   webpack(config, { webpack }) {
+//     const prod = process.env.NODE_ENV === "production";
+//     return {
+//       ...config,
+//       mode: prod ? "production" : "development",
+//       devtool: prod ? "hidden-source-map" : "eval",
+
+//       // reactStrictMode: false,
+//       // swcMinify: true,
+//       // compiler: {
+//       //   // ssr and displayName are configured by default
+//       //   styledComponents: true,
+//       // },
+//     };
+//   },
+// });
